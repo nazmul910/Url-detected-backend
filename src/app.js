f3152a1af81876d0e298bde81bc8383bcea7cc81
nazmul910 <islamnasir910@gmail.com>
2026-04-26 20:46:12 +0600

    refactor: enhance database connection handling and improve app initialization

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import domainRoutes from "./routes/domainRoutes.js";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// routes
+
+app.use(async (req, res, next) => {
+  await connectDB();
+  next();
+});
+
 app.use("/api", domainRoutes);
 
 export default app;
--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 export const connectDB = async () => {
+  if (isConnected) return; 
+
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
     console.log("MongoDB Connected ✅");
   } catch (error) {
     console.error("DB Error:", error.message);
-    process.exit(1);
+    throw error;
   }
 };
